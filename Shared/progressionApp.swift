58b547292688f601e5b95a82620c6446fa48cdf5
Dan Tame <hello@danta.me>
2022-06-05 14:24:56 +0100

    Initial Commit

---
+++ Shared/progressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressionApp.swift
+//  Shared
+//
+//  Created by Dan Tame on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct progressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
