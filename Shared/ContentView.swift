d87589ba6f5220fef195111e6d909186380b7ddf
Dan Tame <hello@danta.me>
2022-06-19 21:04:22 +0100

    Adds chord and pitch midi interface

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,197 @@
 //
 
 import SwiftUI
+import AudioKit
+import CoreMIDI
+import Foundation
+import MusicTheory
 
 struct ContentView: View {
+    @StateObject var conductor: MidiConductor = MidiConductor()
+    @State private var selectedPort1Uid: MIDIUniqueID?
+    @State private var selectedPort2Uid: MIDIUniqueID?
+    @State private var selectedScaleType: ScaleType = ScaleType.major
+    @State private var selectedKey: Key = Key(type: .c) {
+        didSet {
+            setScale()
+            setPitches()
+            setChords()
+        }
+    }
+    
+    @State private var selectedOctave: Int = 4
+    
+    @State private var scale: Scale = Scale(type: ScaleType.major, key: Key(type: .c))
+    @State private var pitches: [PitchIdentifiable] = Scale(type: ScaleType.major, key: Key(type: .c)).pitches(octaves: [1,2,3,4,5]).map({PitchIdentifiable(pitch: $0)}).reversed()
+    
+    @State private var chords: [ChordIdentifiable] =
+    Scale.HarmonicField.all.flatMap({field in
+        Scale(type: ScaleType.major, key: Key(type: .c)).harmonicField(for: field).compactMap({$0 != nil ? ChordIdentifiable(chord: $0) : nil})
+    })
+    
+    struct ScaleTypeIdentifiable: Identifiable {
+        let id = UUID()
+        let scale: ScaleType
+    }
+    
+    struct KeyIdentifiable: Identifiable {
+        let id = UUID()
+        let key: Key
+    }
+    
+    struct PitchIdentifiable: Identifiable {
+        let id = UUID()
+        let pitch: Pitch
+    }
+    
+    struct ChordIdentifiable: Identifiable {
+        let id = UUID()
+        let chord: Chord?
+    }
+    
+    func setScale() {
+        scale = Scale(type: selectedScaleType, key: selectedKey)
+    }
+    
+    func setChords() {
+        chords = Scale.HarmonicField.all.flatMap({field in
+            scale.harmonicField(for: field).compactMap({$0 != nil ? ChordIdentifiable(chord: $0) : nil})
+        })
+    }
+    
+    func setPitches() {
+        pitches = scale.pitches(octaves: [1,2,3,4,5]).map({ PitchIdentifiable(pitch: $0)})
+    }
+    
+    var scales: [ScaleTypeIdentifiable] = ScaleType.all.map({ ScaleTypeIdentifiable(scale: $0) })
+    var keys: [KeyIdentifiable] = Key.keysWithFlats.map({ KeyIdentifiable(key: $0) })
+    
+    var pitchColumns: [GridItem] =
+    Array(repeating: .init(.fixed(100), spacing: 10), count: 7)
+    
+    var chordColumns: [GridItem] =
+    Array(repeating: .init(.fixed(100), spacing: 10), count: 5)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Picker(selection: $selectedPort1Uid, label:
+                        Text("Destination Ports:")
+                ) {
+                    Text("All")
+                        .tag(nil as MIDIUniqueID?)
+                    ForEach(0..<conductor.destinationNames.count, id: \.self) { index in
+                        
+                        Text("\(conductor.destinationNames[index])")
+                            .tag(conductor.destinationUIDs[index] as MIDIUniqueID?)
+                    }
+                }.pickerStyle(.segmented)
+                Picker(selection: $selectedPort2Uid, label:
+                        Text("Virtual Output Ports:")
+                ) {
+                    Text("All")
+                        .tag(nil as MIDIUniqueID?)
+                    ForEach(0..<conductor.virtualOutputUIDs.count, id: \.self) { index in
+                        Text("\(conductor.virtualOutputNames[index])")
+                            .tag(conductor.virtualOutputUIDs[index] as MIDIUniqueID?)
+                    }
+                }.pickerStyle(.segmented)
+            }
+            Divider()
+            Text("Selected Scale: \(selectedScaleType.description)")
+            Picker(selection: $selectedScaleType, label: Text("Scales:")) {
+                ForEach(scales) { scale in
+                    Text(scale.scale.description).tag(scale.scale)
+                }
+            }.onChange(of: selectedScaleType) { _ in setScale(); setPitches(); setChords() }
+            
+            Divider()
+            Text("Selected Key: \(selectedKey.description)")
+            HStack {
+                ForEach(keys) { key in
+                    Button(action: { selectedKey = key.key; setPitches() }) {
+                        Text(key.key.description).padding().frame(width:75, height: 50).cornerRadius(10.0).foregroundColor(.white).background(Rectangle().fill(.red))
+                    }
+                    
+                }
+            }
+            Divider()
+            HStack {
+                
+                LazyVGrid(columns: pitchColumns, alignment: .leading, spacing: 10) {
+                    ForEach(pitches) { pitch in
+                        Button(action: {}) {
+                            Text(pitch.pitch.description).multilineTextAlignment(.center).padding().frame(width: 100, height: 100).cornerRadius(10.0).foregroundColor(.white).background(
+                                Rectangle().fill(.blue))
+                        }
+                        .onLongPressGesture(minimumDuration: 0, perform: {}, onPressingChanged: {isPressing in isPressing ? sendMidiDown(pitch: pitch.pitch) : sendMidiUp(pitch: pitch.pitch)})
+                    }
+                    
+                    
+                }
+                
+                LazyVGrid(columns: chordColumns, spacing: 10) {
+                    ForEach(chords) { chord in
+                        Button(action: { }) {
+                            Text(chord.chord!.description).multilineTextAlignment(.center).frame(width: 100, height: 100).cornerRadius(10.0).foregroundColor(.white).background(
+                                Rectangle().fill(.green)).font(.system(size: 14))
+                        }
+                        .onLongPressGesture(minimumDuration: 0, perform: {}, onPressingChanged: {isPressing in isPressing ? sendChordDown(chord: chord.chord!) : sendChordUp(chord: chord.chord!)})
+                    }
+                    
+                }
+            }
+        }.padding()
+        
+    }
+    
+    func sendChordDown(chord: Chord) {
+        let pitches = chord.pitches(octave: 2)
+        
+        for pitch in pitches {
+            sendMidiDown(pitch: pitch)
+        }
+    }
+    
+    func sendChordUp(chord: Chord) {
+        let pitches = chord.pitches(octave: 2)
+        
+        for pitch in pitches {
+            sendMidiUp(pitch: pitch)
+        }
+    }
+    
+    func sendMidiDown(pitch: Pitch) {
+        let eventToSend = StMIDIEvent(statusType: MIDIStatusType.noteOn.rawValue,
+                                      channel: 0,
+                                      data1: MIDIByte(pitch.rawValue),
+                                      data2: 90)
+        if selectedPort1Uid != nil {
+            conductor.sendEvent(eventToSend: eventToSend, portIDs: [selectedPort1Uid!])
+        } else {
+            conductor.sendEvent(eventToSend: eventToSend, portIDs: nil)
+        }
+    }
+    
+    func sendMidiUp(pitch: Pitch) {
+        let eventToSend = StMIDIEvent(statusType: MIDIStatusType.noteOff.rawValue,
+                                      channel: 0,
+                                      data1: MIDIByte(pitch.rawValue),
+                                      data2: 90)
+        if selectedPort1Uid != nil {
+            conductor.sendEvent(eventToSend: eventToSend, portIDs: [selectedPort1Uid!])
+        } else {
+            conductor.sendEvent(eventToSend: eventToSend, portIDs: nil)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPad Pro (12.9-inch) (5th generation)")
+                .previewInterfaceOrientation(.landscapeLeft)
+        }
     }
 }
--- Shared/progressionApp.swift
+++ Shared/progressionApp.swift
@@ -6,12 +6,127 @@
 //
 
 import SwiftUI
+import AudioKit
+import Foundation
+import CoreMIDI
 
 @main
 struct progressionApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+struct StMIDIEvent: Decodable, Encodable {
+    var statusType: Int
+    var channel: MIDIChannel
+    var data1: MIDIByte
+    var data2: MIDIByte?
+    var portUniqueID: MIDIUniqueID?
+}
+
+class MidiConductor: ObservableObject {
+    let inputUIDDevelop: Int32 = 1_200_000
+    let outputUIDDevelop: Int32 = 1_500_000
+    let inputUIDMain: Int32 = 2_200_000
+    let outputUIDMain: Int32 = 2_500_000
+    let midi = MIDI()
+    
+    @Published var outputIsOpen: Bool = false {
+        didSet {
+            print("outputIsOpen: \(outputIsOpen)")
+            if outputIsOpen {
+                openOutputs()
+            } else {
+                midi.closeOutput()
+            }
+        }
+    }
+    @Published var outputPortIsSwapped: Bool = false
+    
+    var destinationNames: [String] {
+        midi.destinationNames
+    }
+    var destinationUIDs: [MIDIUniqueID] {
+        midi.destinationUIDs
+    }
+    var destinationInfos: [EndpointInfo] {
+        midi.destinationInfos
+    }
+    var virtualOutputNames: [String] {
+        midi.virtualOutputNames
+    }
+    var virtualOutputUIDs: [MIDIUniqueID] {
+        midi.virtualOutputUIDs
+    }
+    var virtualOutputInfos: [EndpointInfo] {
+        midi.virtualOutputInfos
+    }
+    
+    init() {
+        //        midi.createVirtualOutputPorts(count: 1, uniqueIDs: [outputUIDDevelop])
+        midi.createVirtualOutputPorts(count: 1, uniqueIDs: [outputUIDMain], names: ["Virtual Output Port"])
+        openOutputs()
+    }
+    
+    func swapVirtualOutputPorts (withUID uid: [MIDIUniqueID]?) -> [MIDIUniqueID]? {
+        if uid != nil {
+            if outputPortIsSwapped {
+                switch uid {
+                case [outputUIDMain]: return [inputUIDMain]
+                case [outputUIDDevelop]: return [inputUIDDevelop]
+                    
+                default:
+                    return uid
+                }
+            }
+        }
+        return uid
+    }
+    
+    
+    func sendEvent(eventToSend event: StMIDIEvent, portIDs: [MIDIUniqueID]?) {
+        let portIDs2: [MIDIUniqueID]? = swapVirtualOutputPorts(withUID: portIDs)
+        if portIDs2 != nil {
+            print("sendEvent, port: \(portIDs2![0].description)")
+        }
+        switch event.statusType {
+        case MIDIStatusType.controllerChange.rawValue:
+            //                print("sendEvent controllerChange, port: \(portIDs2![0].description)")
+            midi.sendControllerMessage(event.data1,
+                                       value: event.data2 ?? 0,
+                                       channel: event.channel,
+                                       endpointsUIDs: portIDs2)
+        case MIDIStatusType.programChange.rawValue:
+            //                print("sendEvent programChange, port: \(portIDs2![0].description)")
+            midi.sendEvent(MIDIEvent(programChange: event.data1,
+                                     channel: event.channel),
+                           endpointsUIDs: portIDs2)
+        case MIDIStatusType.noteOn.rawValue:
+            //                print("sendEvent noteOn, port: \(portIDs2![0].description)")
+            midi.sendNoteOnMessage(noteNumber: event.data1,
+                                   velocity: event.data2 ?? 0,
+                                   channel: event.channel,
+                                   endpointsUIDs: portIDs2)
+        case MIDIStatusType.noteOff.rawValue:
+            //                print("sendEvent noteOn, port: \(portIDs2![0].description)")
+            midi.sendNoteOffMessage(noteNumber: event.data1,
+                                    channel: event.channel,
+                                    endpointsUIDs: portIDs2)
+        default:
+            // Do Nothing
+            ()
+        }
+    }
+    
+    func openOutputs () {
+        for uid in midi.destinationUIDs {
+            midi.openOutput(uid: uid)
+        }
+        for uid in midi.virtualOutputUIDs {
+            midi.openOutput(uid: uid)
         }
     }
 }
